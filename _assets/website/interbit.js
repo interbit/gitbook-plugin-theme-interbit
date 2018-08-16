102e3a24798d0409a154eb15d568e1f725fcd6d2
Ewan Edwards <ewan@blockchaintechltd.com>
2018-08-16 15:43:12 -0700

    Add medium-zoom for pleasant screenshot viewing.

--- _assets/website/interbit.js
+++ _assets/website/interbit.js
@@ -1,22 +1,27 @@
 require(['gitbook', 'jquery'], function(gitbook, $) {
 
-    function toggleSearchFocus() {
-        var $search = $('#book-search-input');
+  function toggleSearchFocus() {
+    var $search = $('#book-search-input');
 
-        $search.on('focus blur', 'input', function() {
-            $search.toggleClass('focus');
-        });
-    }
-    function removePublishedBy() {
-        var link = $('.gitbook-link');
-        link.remove();
-    }
-
-    gitbook.events.on('start', function() {
-        toggleSearchFocus();
+    $search.on('focus blur', 'input', function() {
+      $search.toggleClass('focus');
     });
+  }
+  function removePublishedBy() {
+    var link = $('.gitbook-link');
+    link.remove();
+  }
 
-    gitbook.events.on('page.change', function() {
-        removePublishedBy();
+  gitbook.events.on('start', function() {
+    toggleSearchFocus();
+  });
+
+  gitbook.events.on('page.change', function() {
+    removePublishedBy();
+
+    mediumZoom('span.frame img', {
+      background: "#000",
+      scrollOffset: 24
     });
+  });
 });
