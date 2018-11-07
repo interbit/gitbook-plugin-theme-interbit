42e1eaded4163ac70718ed8f7f18b7da704ac7df
Ewan Edwards <ewan@blockchaintechltd.com>
2018-11-07 11:29:13 -0800

    Add interbit-ui-components as a dependency, copy its assets into the theme on install, and declare the assets so GitBook can use them.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = {
-    book: {
-        assets: './_assets/website/',
-        js: [
-            'interbit.js'
-        ],
-        css: [
-            'interbit.css'
-        ]
-    }
+  book: {
+    assets: './_assets/website/',
+    js: [
+      'interbit.js',
+      'interbit-ui-components.js'
+    ],
+    css: [
+      'interbit.css',
+      'interbit-ui-components.css'
+    ]
+  }
 };
---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+DIST=node_modules/interbit-ui-components/dist
+cp $DIST/interbit-ui-components.js _assets/website/
+cp $DIST/css/interbit.css _assets/website/interbit-ui-components.css
